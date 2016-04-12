abe1d0cc5e3f0d42395300397cd166f5d1af510f
Eric Quanz <eric.quanz@gmail.com>
2016-04-12 14:36:16 -0300

    add titleize passing definition

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -17,3 +17,19 @@ end
 def first_word phrase
   phrase.split(" ")[0]
 end
+
+def titleize phrase
+  little_words = ['and', 'over', 'the']
+  capitalized = []
+  words = phrase.downcase.split
+
+  words.each_with_index do |word, index|
+    if little_words.include? word and index != 0
+      capitalized << word
+    else
+      capitalized << word.capitalize
+    end
+  end
+
+  capitalized.join(" ")
+end
